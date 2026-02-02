123b83684ca0aaa803dc1660e328ab668177e7d0
Marc Blank <marc.blank@live.com>
2026-02-02 11:11:14 -0800

    4.0.0.11 - new upgrader

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //Should be incremented for each new release build of a plugin
 
 // Odd minor releases for Beta
-[assembly: AssemblyVersion("4.0.0.10")]
-[assembly: AssemblyFileVersion("4.0.0.10")]
+[assembly: AssemblyVersion("4.0.0.11")]
+[assembly: AssemblyFileVersion("4.0.0.11")]
 
 // [MANDATORY] The name of your plugingit st
 [assembly: AssemblyTitle("Sequencer Powerups")]
