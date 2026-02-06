cc2bb8e372aa98b879eac990bb72ee8befd99df6
Marc Blank <marc.blank@live.com>
2026-02-05 19:57:48 -0800

    Fix Break; remove Annotation + from sidebar

--- When/PowerupsPlusInstructions/Log.cs
+++ When/PowerupsPlusInstructions/Log.cs
@@ -28,11 +28,6 @@ using NINA.Sequencer.Logic;
 
 namespace WhenPlugin.When {
 
-    [ExportMetadata("Name", "Annotation +")]
-    [ExportMetadata("Description", "Add the specified text to the log, expanding any expressions within {}'s")]
-    [ExportMetadata("Icon", "ScriptSVG")]
-    [ExportMetadata("Category", "Powerups")]
-    [Export(typeof(ISequenceItem))]
     [JsonObject(MemberSerialization.OptIn)]
     public class Log : SequenceItem, IValidatable {
 
