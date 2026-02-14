eddaf6f50ea090cf5d1149bea7015a69e9e90751
Marc Blank <marc.blank@live.com>
2026-02-14 14:38:17 -0800

    Also bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 //Should be incremented for each new release build of a plugin
 
 // Odd minor releases for Beta
-[assembly: AssemblyVersion("4.0.0.57")]
-[assembly: AssemblyFileVersion("4.0.0.57")]
+[assembly: AssemblyVersion("4.0.0.58")]
+[assembly: AssemblyFileVersion("4.0.0.58")]
 
 // [MANDATORY] The name of your plugingit st
 [assembly: AssemblyTitle("Sequencer Powerups")]
